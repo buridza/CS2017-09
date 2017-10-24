96d425af2ce842bcac94a6acfd19ad8d0f067650
Elena <serzhan.helen@gmail.com>
2017-10-24 12:12:44 +0300

    less 9 complete А-В

--- src/by/it/sc02_morning/serzhan/lesson06/TaskB3.java
+++ src/by/it/sc02_morning/serzhan/lesson06/TaskB3.java
@@ -23,23 +23,18 @@ public class TaskB3 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int maximum = 0;
-        int minimum = 0;
         int n = Integer.parseInt(reader.readLine());
-        if (n > 0)
+        if (n > 0) {
             maximum = Integer.parseInt(reader.readLine());
-        minimum = Integer.parseInt(reader.readLine());
-        for (int x = 0; x < n-2 ; x++) {
-            int s = Integer.parseInt(reader.readLine());
-            if (s > maximum) maximum = s;
-            if (s < minimum) minimum = s;
+            for (int x = 0; x < n - 2; x++) {
+                int s = Integer.parseInt(reader.readLine());
+                if (s > maximum) maximum = s;
+            }
+            if (maximum == 0) {
+                maximum = 1;
+            }
+            System.out.println(maximum);
         }
-        if (maximum == 0) {
-            maximum = 1;
-        }
-        System.out.println(maximum);
-        //else minimum=1;
-        System.out.println(minimum);
-       // System.out.println(maximum);
-
     }
 }
+
