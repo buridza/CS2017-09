1ee94277f96531c11608599b9c6617ecc2f69cc6
travel.vi@mail.ru <travel.vi@mail.ru>
2017-10-29 13:15:04 +0200

    Runets. Lesson 9, A-B

---
+++ src/by/it/sc04_evening_tue_thu/runec/lesson10/A1VideoRegistrator.java
@@ -0,0 +1,59 @@
+package by.it.sc04_evening_tue_thu.runec.lesson10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+Даны события events и интервал workDuration работы регистратора.
+
+Реализуйте метод calcStartTimes, так,
+чтобы общее число включений регистратора на
+указанный период времени (1) было минимальным,
+но все события events при этом были зарегистрированы.
+Одно и то же событие не может быть зарегистрировано
+в двух разных включениях регистратора
+
+Метод возвращает список включений регистратора
+Алгоритм жадный. Для реализации обдумайте надежный шаг.
+*/
+
+public class A1VideoRegistrator {
+
+    public static void main(String[] args) {
+        A1VideoRegistrator instance=new A1VideoRegistrator();
+        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        double workDuration=1;
+        //рассчитаем моменты старта, с длиной сеанса 1
+        List<Double> starts=instance.calcStartTimes(events,workDuration);
+        //покажем полученные моменты старта
+        System.out.println(starts);
+    }
+
+    //модификаторы доступа опущены для возможности тестирования
+    List<Double> calcStartTimes(double[] events, double workDuration){
+        //events - события которые нужно зарегистрировать
+        //workDuration время работы видеокамеры после старта
+        List<Double> result;
+        result = new ArrayList<>();
+        /*hint: сортировка Arrays.sort обеспечит скорость алгоритма
+                C*(n log n) + C1*n = O(n log n)
+                а сортировка пузырьком
+                C*(n * n) + C1*n = O(n*n) */
+        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
+        //Ваше решение тут
+        int i=0;                              //i - это индекс события events[i]
+        Arrays.sort(events);                  //подготовка к жадному поглощению массива событий
+
+         while (i<events.length)   {          //пока есть незарегистрированные события
+          double start=events[i];             //получим одно событие по левому краю
+          double stop=start+workDuration;     //и запомним время старта видеокамеры
+          result.add(start);
+          while (i<events.length&&events[i]<=stop) //вычислим момент окончания работы видеокамеры
+           i++; }                                  //и теперь пропустим все покрываемые
+                                              //за время работы видеокамеры события
+                                              //увеличивая индекс.
+
+
+        return result;                        //вернем итог
+    }
+}
---
+++ src/by/it/sc04_evening_tue_thu/runec/lesson10/C1GreedyKnapsack.java
@@ -0,0 +1,84 @@
+package by.it.sc04_evening_tue_thu.runec.lesson10;
+/*
+Соберите рюкзак.
+
+Из файла или из консоли вводятся числа (конкретные цифры - для примера)
+1) объем рюкзака 60
+2) число возможных предметов 4
+3) сам набор предметов
+    100 50
+    120 30
+    100 50
+    100 50
+Необходимо собрать наиболее дорогой вариант рюкзака для этого объема
+Решение приведите в методе calc
+
+Предметы можно резать на кусочки (т.е. алгоритм будет жадным)
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C1GreedyKnapsack {
+
+    double calc(File source) throws FileNotFoundException {
+        Scanner input = new Scanner(source);
+        int n = input.nextInt();      //сколько предметов в файле
+        int W = input.nextInt();      //какой вес у рюкзака
+        Item[] items = new Item[n];   //получим список предметов
+        for (int i = 0; i < n; i++) { //создавая каждый конструктором
+            items[i] = new Item(input.nextInt(), input.nextInt());
+        }
+        //покажем предметы
+        for (Item item:items) {
+            System.out.println(item);
+        }
+        System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n",n,W);
+        Arrays.sort(items);
+
+       // int cost=items[0].cost;
+        //int cost1=items[1].cost;
+       // int cost2=items[2].cost*(W-items[0].weight-items[1].weight)/(items[2].weight);
+        double result = 0;
+        int ves=0;
+                for (int i = 0; ; i++) {
+                        if((ves+items[i].weight)<W){
+                                ves+=items[i].weight;
+                                result+=items[i].cost;
+                            }
+                        else{ result+=items[i].cost/items[i].weight*(W-ves); break;}
+                    }
+
+
+
+
+
+        //тут необходимо реализовать решение задачи
+        //итогом является максимально воможная стоимость вещей в рюкзаке
+        //вещи можно резать на кусочки (непрерывный рюкзак)
+
+        //реализуйте алгоритм сбора рюкзака
+        //будет особенно хорошо, если с собственной сортировкой
+        //кроме того, можете описать свой компаратор в классе Item
+
+        //ваше решение.
+
+
+
+
+
+        System.out.printf("Удалось собрать рюкзак на сумму %f\n",result);
+        return result;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        long startTime = System.currentTimeMillis();
+        String root=System.getProperty("user.dir")+"/src/";
+        File f=new File(root+"by/it/akhmelev/lesson10/greedyKnapsack.txt");
+        double costFinal=new C1GreedyKnapsack().calc(f);
+        long finishTime = System.currentTimeMillis();
+        System.out.printf("Общая стоимость %f (время %d)",costFinal,finishTime - startTime);
+    }
+}
