9ce1c05e9afc14e6baf364595cad4587be6a38f7
Opanovich <opanovichmv@gmail.com>
2017-10-31 15:01:10 +0300

    All done for lesson11

---
+++ src/by/it/sc02_morning/opanovich/lesson11/B_MergeSort.java
@@ -0,0 +1,84 @@
+package by.it.sc02_morning.opanovich.lesson11;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Реализуйте сортировку слиянием для одномерного массива.
+Сложность алгоритма должна быть не хуже, чем O(n log n)
+
+Первая строка содержит число 1<=n<=10000,
+вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
+Необходимо отсортировать полученный массив.
+
+Sample Input:
+5
+2 3 9 2 9
+Sample Output:
+2 2 3 9 9
+*/
+public class B_MergeSort {
+
+
+    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //размер массива
+        int n = scanner.nextInt();
+        //сам массив
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = scanner.nextInt();
+        System.out.println("Прочитан массив a="+ Arrays.toString(a));
+        // тут реализуйте сортировку слиянием
+        // https://ru.wikipedia.org/wiki/Сортировка_слиянием
+        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
+
+        a = mergeSort(a, 0, a.length - 1);
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return a;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/sc02_morning/opanovich/lesson11/dataB.txt");
+        B_MergeSort instance = new B_MergeSort();
+        //long startTime = System.currentTimeMillis();
+        int[] result=instance.getMergeSort(stream);
+        System.out.println("Отсортированный массив "+ Arrays.toString(result));
+        //long finishTime = System.currentTimeMillis();
+    }
+
+    private static int[] mergeSort(int[] a, int left, int right) {
+
+        if (left == right) {
+            return new int[]{a[left]};
+        }
+        int middle = (left + right) / 2;
+        return merge(mergeSort(a, left, middle), mergeSort(a, middle + 1, right));
+    }
+
+    private static int[] merge(int[] a, int[] b) {
+
+        int[] result = new int[a.length + b.length];
+        for (int i = 0, conA = 0, conB = 0; i < result.length; i++) {
+            if (conA == a.length) {
+                result[i] = b[conB++];
+            } else if (conB == b.length) {
+                result[i] = a[conA++];
+            } else if (a[conA] > b[conB]) {
+                result[i] = b[conB++];
+            } else {
+                result[i] = a[conA++];
+            }
+        }
+        return result;
+    }
+
+}
