a017eaa7353ddfa225b0ac12aba5717ae8e1f365
Khmelov <375336849110@tut.by>
2017-11-01 23:32:06 +0300

    Added lesson12 + tests

---
+++ src/by/it/akhmelev/lesson12/B_CountSort.java
@@ -0,0 +1,51 @@
+package by.it.akhmelev.lesson12;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Первая строка содержит число 1<=n<=10000, вторая - n натуральных чисел, не превышающих 10.
+Выведите упорядоченную по неубыванию последовательность этих чисел.
+
+При сортировке реализуйте метод с вычислительной сложностью не более, чем O(n)
+*/
+
+public class B_CountSort {
+
+
+    int[] countSort(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //размер массива
+        int n = scanner.nextInt();
+        int[] points=new int[n];
+
+        //читаем точки
+        for (int i = 0; i < n; i++) {
+            points[i]=scanner.nextInt();
+        }
+        //тут реализуйте логику задачи с применением сортировки подсчетом
+
+
+
+
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return points;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/akhmelev/lesson12/dataB.txt");
+        B_CountSort instance = new B_CountSort();
+        int[] result=instance.countSort(stream);
+        for (int index:result){
+            System.out.print(index+" ");
+        }
+    }
+
+}
