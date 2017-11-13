d499679b23391f96e966e5e85c14b939c60bb202
computer <travel.vi@mail.ru>
2017-11-13 21:08:59 +0200

    Runets. Lesson 12 (A,B)

--- src/by/it/sc04_evening_tue_thu/runec/lesson12/A_QSort.java
+++ src/by/it/sc04_evening_tue_thu/runec/lesson12/A_QSort.java
@@ -19,8 +19,8 @@ import java.util.Scanner;
 Вам необходимо определить для каждого события сколько камер его записали.
 
 В первой строке задано два целых числа:
-    число включений камер (отрезки) 1<=n<=50000
-    число событий (точки) 1<=m<=50000.
+число включений камер (отрезки) 1<=n<=50000
+число событий (точки) 1<=m<=50000.
 
 Следующие n строк содержат по два целых числа ai и bi (ai<=bi) -
 координаты концов отрезков (время работы одной какой-то камеры).
@@ -31,13 +31,13 @@ import java.util.Scanner;
 
 Для каждой точки в порядке их появления во вводе выведите,
 скольким отрезкам она принадлежит.
-    Sample Input:
-    2 3
-    0 5
-    7 10
-    1 6 11
-    Sample Output:
-    1 0 0
+Sample Input:
+2 3
+0 5
+7 10
+1 6 11
+Sample Output:
+1 0 0
 
 Подсказки и проверка решения:
 1) самый быстрый алгоритм имеет сложность O(n log n)
@@ -47,22 +47,50 @@ import java.util.Scanner;
 
 public class A_QSort {
 
+//events point+segmenr
+
+    private class Event implements Comparable<Event>{
+        int time;
+        int type; //-1 - off, 0 - point, -1 - on
+
+        public Event (int time, int type){
+            this.time=time;
+            this.type=type;
+        }
+
+        @Override
+
+        public String toString(){
+            return "e("+time+", "+type+") ";
+        }
+
+        public int compareTo(Event otherEvent) {
+
+            int res = this.time- otherEvent.time;
+            if (res==0)
+                res = this.type+otherEvent.type;
+// return this.time-otherEvent.time;
+            return res;
+
+        }
+    }
+
     //отрезок
-    private class Segment  implements Comparable<Segment>{
+    private class Segment implements Comparable<Segment>{
         int start;
         int stop;
 
         Segment(int start, int stop){
             this.start = start;
             this.stop = stop;
-            //тут можно доделать конструктор на случай если
-            //концы отрезков придут в обратном порядке
+//тут можно доделать конструктор на случай если
+//концы отрезков придут в обратном порядке
         }
 
         @Override
         public int compareTo(Segment otherSegment) {
-            //подумайте, что должен возвращать компаратор отрезков
-            //и нужен ли он вообще.
+//подумайте, что должен возвращать компаратор отрезков
+//и нужен ли он вообще.
             return 0;
         }
 
@@ -71,47 +99,101 @@ public class A_QSort {
             return "("+start +":" + stop +')';
         }
     }
+    private int partition(int[] a, int left, int right) {
+        int x=a[left];
+        int j=left;
+        for (int i=left+1; i<=right; i++){
 
+            if (a[i]<=x){
+                j++;
+                int tmp=a[j];
+                a[j]=a[i];
+                a[i]=tmp;
+            }
+
+        }
+
+        int tmp=a[j];
+        a[j]=a[left];
+        a[left]=tmp;
+        return j;
+    }
+
+    private void qSort (int [] a, int left, int right){
+
+        if (left>=right)
+            return;;
+
+        int m=partition(a,left,right);
+        qSort(a,left,m-1);
+        qSort(a,m+1,right);
+
+    }
+
+    private void qSort (int[] a){
+        qSort(a,0,a.length-1);
+
+    }
 
     int[] getAccessory(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
+//подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        //число отрезков отсортированного массива
+//!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!
+//число отрезков отсортированного массива
         int n = scanner.nextInt();
         Segment[] segments=new Segment[n];
-        //число точек
+//число точек
         int m = scanner.nextInt();
         int[] points=new int[m];
         int[] result=new int[m];
 
-        //читаем сами отрезки
+        Event[] events=new Event[2*n+m];
+        int counterEvent=0;
+
+//читаем сами отрезки
         for (int i = 0; i < n; i++) {
-            //читаем начало и конец каждого отрезка
+//читаем начало и конец каждого отрезка
             segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
+            events[counterEvent++] =new Event(segments[i].start,+1);
+            events[counterEvent++] =new Event(segments[i].stop,-1);
+
         }
         System.out.println("segments="+ Arrays.toString(segments));
-        //читаем точки
+//читаем точки
         for (int i = 0; i < m; i++) {
             points[i]=scanner.nextInt();
+            events[counterEvent++]= new Event(points[i],0);
         }
         System.out.println("points="+ Arrays.toString(points));
-        //тут реализуйте логику задачи
-        //ОБЯЗАТЕЛЬНО с применением быстрой сортировки
+//тут реализуйте логику задачи
+//ОБЯЗАТЕЛЬНО с применением быстрой сортировки
+        Arrays.sort(events);
 
+        System.out.println("Sorted points="+Arrays.toString(events));
 
+        int p=0;
+        int camOn=0;
+        for (int i=0; i<events.length; i++){
+            if (events[i].type==0) {
+                result[p++] = camOn;
+            }
+            else{
+                camOn=camOn+events[i].type;}
 
+        }
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        System.out.println("Sorted points="+Arrays.toString(points));
+
+//!!!!!!!!!!!!!!!!!!!!!!!!! КОНЕЦ ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
 
-
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/akhmelev/lesson12/dataA.txt");
+        InputStream stream = new FileInputStream(root + "by/it/akhmelev/lesson12/dataA.txt"); //поменять на свое
         A_QSort instance = new A_QSort();
         int[] result=instance.getAccessory(stream);
+
         System.out.println("result="+ Arrays.toString(result));
     }
 
