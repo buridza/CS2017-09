ab201ad44cebe9ebdcf5ee65bd9346ef18f20d3a
Khmelov <375336849110@tut.by>
2017-11-13 17:34:57 +0300

    Added lesson13 (complete)

--- src/by/it/akhmelev/lesson13/TaskA1.java
+++ src/by/it/akhmelev/lesson13/TaskA1.java
@@ -16,24 +16,6 @@ package by.it.akhmelev.lesson13;
 // отсортированные зарплаты с надбавкой в 50% (используйте Arrays.toString)
 // сумму с этой надбавкой.
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class TaskA1 {
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        double[] sal=new double[12];
-        for (int i = 0; i < 12; i++)
-            sal[i]=scanner.nextDouble();
-        Salary salary=new Salary(sal);
-        System.out.println(salary);
-        System.out.println(salary.getSum());
 
-        System.out.println(Arrays.toString(salary.getSalary(20)));
-        System.out.println(salary.getSum(20));
-
-        salary.sort();
-        System.out.println(Arrays.toString((salary.getSalary(50))));
-        System.out.println(salary.getSum(50));
-    }
 }
