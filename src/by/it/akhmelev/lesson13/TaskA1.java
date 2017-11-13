5e529887ecafaee07c5ea15715c3b0cfea57bca3
Khmelov <375336849110@tut.by>
2017-11-13 17:34:28 +0300

    Added lesson13 (+tests)

--- src/by/it/akhmelev/lesson13/TaskA1.java
+++ src/by/it/akhmelev/lesson13/TaskA1.java
@@ -1,20 +1,39 @@
 package by.it.akhmelev.lesson13;
 
 //напишите три статических метода,
-//один метод double sum(double[] salary) - считает сумму всех зарплат.
-//второй double[] add(double[] salary, double percent) вычисляет новый массив с надбавкой в percent%.
-//третий void sort(double[] salary) сортирует массив salary.
+//один метод static double sum(double[] salary) - считает сумму всех зарплат.
+//второй static double[] add(double[] salary, double percent) вычисляет новый массив с надбавкой в percent%.
+//третий static void sort(double[] salary) сортирует массив salary.
 
 // в методе main
 // с консоли вводите заработные платы в массив размером в 12 элементов
 // выведите на консоль по очереди:
 
-// неотсортированные зарплаты без надбавок
+// неотсортированные зарплаты без надбавок (используйте Arrays.toString)
 // сумму без надбавок.
-// неотсортированные зарплаты с надбавкой в 20%
+// неотсортированные зарплаты с надбавкой в 20% (используйте Arrays.toString)
 // сумму с этой надбавкой.
-// отсортированные зарплаты с надбавкой в 50%
+// отсортированные зарплаты с надбавкой в 50% (используйте Arrays.toString)
 // сумму с этой надбавкой.
-public class TaskA1 {
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        double[] sal=new double[12];
+        for (int i = 0; i < 12; i++)
+            sal[i]=scanner.nextDouble();
+        Salary salary=new Salary(sal);
+        System.out.println(salary);
+        System.out.println(salary.getSum());
+
+        System.out.println(Arrays.toString(salary.getSalary(20)));
+        System.out.println(salary.getSum(20));
+
+        salary.sort();
+        System.out.println(Arrays.toString((salary.getSalary(50))));
+        System.out.println(salary.getSum(50));
+    }
 }
---
+++ src/by/it/akhmelev/lesson13/Testing13.java
@@ -0,0 +1,193 @@
+package by.it.akhmelev.lesson13;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+@SuppressWarnings("all") //море warnings. всех прячем.
+
+//поставьте курсор на следующую строку и нажмите Ctrl+Shift+F10
+public class Testing13 {
+
+    @Test(timeout = 500)
+    public void testTaskA1() throws Exception {
+        run("12\n11\n10\n9\n8\n7\n6\n5\n4\n3\n2\n1").
+                include("[12.0, 11.0, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]").
+                include("78.0").
+                include("[14.4, 13.2, 12.0, 10.8, 9.6, 8.4, 7.2, 6.0, 4.8, 3.6, 2.4, 1.2]").
+                include("93.6").
+                include("[1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0, 16.5, 18.0]").
+                include("117.0");
+    }
+
+    @Test(timeout = 500)
+    public void testTaskB1() throws Exception {
+        if (findClass("Salary")==null){
+            fail("Нет класса Salary");
+        }
+        run("12\n11\n10\n9\n8\n7\n6\n5\n4\n3\n2\n1").
+                include("[12.0, 11.0, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]").
+                include("78.0").
+                include("[14.4, 13.2, 12.0, 10.8, 9.6, 8.4, 7.2, 6.0, 4.8, 3.6, 2.4, 1.2]").
+                include("93.6").
+                include("[1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0, 16.5, 18.0]").
+                include("117.0");
+    }
+
+
+
+
+    @Test(timeout = 500)
+    public void testTaskC1() throws Exception {
+        if (findClass("Salary")==null){
+            fail("Нет класса Salary");
+        }
+        run("12\n11\n10\n9\n8\n7\n6\n5\n4\n3\n2\n1").
+                include("[12.0, 11.0, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]").
+                include("78.0").
+                include("[14.4, 13.2, 12.0, 10.8, 9.6, 8.4, 7.2, 6.0, 4.8, 3.6, 2.4, 1.2]").
+                include("93.6").
+                include("[1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0, 16.5, 18.0]").
+                include("117.0");
+    }
+
+
+    /*
+    ===========================================================================================================
+    НИЖЕ ВСПОМОГАТЕЛЬНЫЙ КОД ТЕСТОВ. НЕ МЕНЯЙТЕ В ЭТОМ ФАЙЛЕ НИЧЕГО.
+    Но изучить как он работает - можно, это всегда будет полезно.
+    ===========================================================================================================
+     */
+
+    private Class findClass(String SimpleName) {
+        String full = this.getClass().getName();
+        String dogPath = full.replace(this.getClass().getSimpleName(), SimpleName);
+        try {
+            return Class.forName(dogPath);
+        } catch (ClassNotFoundException e) {
+            fail("\nТест не пройден. Класс " + SimpleName + " не найден.");
+        }
+        return null;
+    }
+
+    //метод находит и создает класс для тестирования
+    //по имени вызывающего его метода, testTaskA1 будет работать с TaskA1
+    private static Testing13 run(String in) {
+        Throwable t = new Throwable();
+        StackTraceElement trace[] = t.getStackTrace();
+        StackTraceElement element;
+        int i = 0;
+        do {
+            element = trace[i++];
+        }
+        while (!element.getMethodName().contains("test"));
+
+        String[] path = element.getClassName().split("\\.");
+        String nameTestMethod = element.getMethodName();
+        String clName = nameTestMethod.replace("test", "");
+        clName = element.getClassName().replace(path[path.length - 1], clName);
+        System.out.println("\n---------------------------------------------");
+        System.out.println("Старт теста для " + clName + "\ninput:" + in);
+        System.out.println("---------------------------------------------");
+        return new Testing13(clName, in);
+    }
+
+    public Testing13() {
+        //Конструктор тестов
+    }
+
+    //Конструктор тестов
+    //    private Testing(String className) {
+    //        this(className, "");
+    //    }
+
+    //Основной конструктор тестов
+    private Testing13(String className, String in) {
+        //this.className = className;
+        Class<?> c = null;
+        try {
+            c = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            fail("Не найден класс " + className);
+        }
+        reader = new StringReader(in); //заполнение ввода
+        InputStream inputStream = new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return reader.read();
+            }
+        };
+        System.setIn(inputStream);   //перехват ввода
+
+        System.setOut(newOut); //перехват стандартного вывода
+        try {
+            Class[] argTypes = new Class[]{String[].class};
+            Method main = c.getDeclaredMethod("main", argTypes);
+            main.invoke(null, (Object) new String[]{});
+
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        System.setOut(oldOut); //возврат вывода
+    }
+
+    //проверка вывода
+    private Testing13 is(String str) {
+        assertTrue("Ожидается такой вывод:\n<---начало---->\n" + str + "<---конец--->",
+                stringWriter.toString().equals(str));
+        return this;
+    }
+
+    private Testing13 include(String str) {
+        assertTrue("Строка не найдена: " + str + "\n", stringWriter.toString().contains(str));
+        return this;
+    }
+
+    private Testing13 exclude(String str) {
+        assertTrue("Лишние данные в выводе: " + str + "\n", !stringWriter.toString().contains(str));
+        return this;
+    }
+
+
+    //переменные теста
+    private StringWriter stringWriter = new StringWriter();
+    private PrintStream oldOut = System.out;
+    private StringReader reader;
+
+
+    //поле для перехвата потока вывода
+    private PrintStream newOut;
+
+    {
+        newOut = new PrintStream(new OutputStream() {
+            private byte bytes[] = new byte[2];
+
+            @Override
+            public void write(int b) throws IOException {
+                if (b < 0) { //ловим и собираем двухбайтовый UTF (первый код > 127, или в байте <0)
+                    if (bytes[0] == 0) { //если это первый байт
+                        bytes[0] = (byte) b; //то запомним его
+                    } else {
+                        bytes[1] = (byte) b; //иначе это второй байт
+                        String s = new String(bytes); //соберем весь символ
+                        stringWriter.append(s); //запомним вывод для теста
+                        oldOut.append(s); //копию в обычный вывод
+                        bytes[0] = 0; //забудем все.
+                    }
+                } else {
+                    char c = (char) b; //ловим и собираем однобайтовый UTF
+                    bytes[0] = 0;
+                    if (c != '\r') {
+                        stringWriter.write(c); //запомним вывод для теста
+                    }
+                    oldOut.write(c); //копию в обычный вывод
+                }
+            }
+        });
+    }
+
+}
